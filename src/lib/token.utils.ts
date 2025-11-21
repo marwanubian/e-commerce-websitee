c3464d1067956446554a33b51d4eee333504195c
Marwa Mahmoud <32708371+marwanubian@users.noreply.github.com>
2025-11-21 22:46:44 +0200

    Update token.utils.ts

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -3,10 +3,11 @@ import { cookies } from 'next/headers'
 import React from 'react'
 
 export  async function getUserToken() {
-    const encodedToken=(await cookies()).get("next-auth.session-token")?.value
-   const decreptToken= await decode({token:encodedToken,secret:process.env.AUTH_SECRET!})
+   const decodedToken=(await cookies()).get("next-auth.session-token")?.value||(await cookies()).get("__Secure-next-auth.session-token")?.value;
+//  const encodedToken=(await cookies()).get("next-auth.session-token")?.value
+   const decreptToken= await decode({token:decodedToken,secret:process.env.AUTH_SECRET!})
    const token= decreptToken?.token
-
    console.log(token,"tooooken")
   return token
 }
+
